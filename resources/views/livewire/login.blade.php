13affbfa059aae7732c380ad5fde33d547387448
Fall codeur <fallmidadi1996@gmail.com>
2023-10-20 10:10:14 +0000

    Recupération

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -6,7 +6,18 @@
                     <div class="col-md-4">
                     </div>
                     <div class="col-md-8 bg-danger">
-                       
+                        
+                            <!-- Nav tabs -->
+                            <ul class="nav nav-tabs nav-justified" role="tablist">
+                              <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Connection</a></li>
+                              <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Inscription</a></li>
+                            </ul>
+                          
+                            <!-- Tab panes -->
+                            <div class="tab-content">
+                              <div role="tabpanel" class="tab-pane active" id="home">...</div>
+                              <div role="tabpanel" class="tab-pane" id="profile">...</div>
+                            </div>
                     </div>
                 </div>
             </div>
