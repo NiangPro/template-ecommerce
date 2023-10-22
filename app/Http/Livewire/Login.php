570597a926e1584988193ece6c948ee7a323fb04
Fall codeur <fallmidadi1996@gmail.com>
2023-10-22 14:26:38 +0000

    recupération

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -2,10 +2,95 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Login extends Component
 {
+
+    public $form1 = [
+        "prenom" => "",
+        "nom" => "",
+        "nationalite" => "",
+        "pays" => "",
+        "pseudo" => "",
+        "adresse" => "",
+        "role" => "",
+        "tel" => "",
+        "tel2" => "",
+        "email" => "",
+        "password" => "",
+    ];
+
+     public $form2 = [
+        "email" => "",
+        "password" => "",
+    ];
+
+    protected  $messages = [
+        'form1.prenom.required' => 'Le prenom est requis',
+        'form1.nom.required' => 'Le nom est requis',
+        'form1.nationalite.required' => 'la nationnalite est requise',
+        'form1.adresse.required' => 'L\'adresse est requis',
+        'form1.tel.required' => 'Le telephone est requis',
+        'form1.tel.regex' => 'Le n° de telephone est invalide',
+        'form1.tel.min' => 'Le n° de telephone doit avoir au minimum 9 chiffres',
+        'form1.tel.max' => 'Le n° de telephone doit avoir au maximum 9 chiffres',
+        'form1.email.required' => 'L\'email est requis',
+        'form1.password.required' => 'Le mot de passe est requis',
+        'form1.role.required' => 'Le role est requis',
+        'form2.email.required' => 'L\'email est requis',
+        'form2.password.required' => 'Le mot de passe est requis',
+    ];
+
+    public function store()
+    {
+        $this->validate([
+            'form1.prenom' => "required|string",
+            'form1.nom' => "required|string",
+            'form1.nationalite' => "required|string",
+            'form1.pays' => "required|string",
+            'form1.pseudo' => "nullable|string",
+            'form1.adresse' => "required|string",
+            'form1.role' => "required|string",
+            'form1.tel' => ['required', 'min:9', 'max:9', 'regex:/^[33|70|75|76|77|78]+[0-9]{7}$/'],
+            'form1.tel2' => "nullable|string",
+            'form1.email' => "required|string",
+            'form1.password' => "required|string",
+        ]);
+
+        User::create([
+            'prenom' => $this->form1['prenom'],
+            'nom' => $this->form1['nom'],
+            'nationalite' => $this->form1['nationalite'],
+            'pays' => $this->form1['pays'],
+            'pseudo' => $this->form1['pseudo'],
+            'adresse' => $this->form1['adresse'],
+            'role' => 'client',
+            'tel' => $this->form1['tel'],
+            'tel2' => $this->form1['tel2'],
+            'email' => $this->form1['email'],
+            'password' => $this->form1['password']
+        ]);
+
+        $this->initForm();
+        return redirect(route('login'));
+    }
+
+    public function initForm()
+    {
+        $this->form1['prenom'] = "";
+        $this->form1['nom'] = "";
+        $this->form1['nationalite'] = "";
+        $this->form1['pays'] = "";
+        $this->form1['pseudo'] = "";
+        $this->form1['adresse'] = "";
+        $this->form1['tel'] = "";
+        $this->form1['tel2'] = "";
+        $this->form1['email'] = "";
+        $this->form1['password'] = "";
+    }
+
     public function render()
     {
         return view('livewire.login')->layout("layouts.app");
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,116 +1,152 @@
-<div>
-   <!-- ...:::: Start Breadcrumb Section:::... -->
-   <div class="breadcrumb-section breadcrumb-bg-color--golden">
-    <div class="breadcrumb-wrapper">
+ <div>
+    <!-- ...:::: Start Breadcrumb Section:::... -->
+    <div class="breadcrumb-section breadcrumb-bg-color--golden">
+        <div class="breadcrumb-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="breadcrumb-title">Bienvenue dans " Makhfuz" veuillez vous connecter ou vous inscrire</h3>
+                        <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
+                            <nav aria-label="breadcrumb">
+                                {{-- <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="shop-grid-sidebar-left.html">Shop</a></li>
+                                    <li class="active" aria-current="page">Login</li>
+                                </ul> --}}
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ...:::: End Breadcrumb Section:::... -->
+    
+    <!-- ...:::: Start Customer Login Section :::... -->
+    <div class="customer-login">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h3 class="breadcrumb-title">Bienvenue dans " Makhfuz" veuillez vous connecter ou vous inscrire</h3>
-                    <div class="breadcrumb-nav breadcrumb-nav-color--black breadcrumb-nav-hover-color--golden">
-                        <nav aria-label="breadcrumb">
-                            {{-- <ul>
-                                <li><a href="index.html">Home</a></li>
-                                <li><a href="shop-grid-sidebar-left.html">Shop</a></li>
-                                <li class="active" aria-current="page">Login</li>
-                            </ul> --}}
-                        </nav>
+                <!--login area start-->
+                <div class="col-lg-4 col-md-4">
+                    <div class="account_form">
+                        <h3>Connexion</h3>
+                        <form wire:submit.prevent="connecter">
+                            <div class="default-form-box">
+                                <label>Email <span>*</span></label>
+                                <input type="text">
+                            </div>
+                            <div class="default-form-box">
+                                <label>Mots de passe <span>*</span></label>
+                                <input type="password">
+                            </div>
+                            <div class="login_submit mb-4">
+                                <button class="btn btn-md btn-black-default-hover mb-4 float-right" type="submit" >Se connecter</button>
+                                <button style="background-color: #ff365d; color: white" class="btn btn-md mb-4 float-left" type="reset">Annuler</button>  
+                                <label class="checkbox-default mb-4" for="offer">
+                                    {{-- <input type="checkbox" id="offer">
+                                    <span>Remember me</span> --}}
+                                </label>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div> <!-- ...:::: End Breadcrumb Section:::... -->
+                <!--login area start-->
 
-<!-- ...:::: Start Customer Login Section :::... -->
-<div class="customer-login mt-n5">
-    <div class="container m-auto">
-        <div class="row">
-            <!--login area start-->
-            {{-- <div class="col-lg-6 col-md-6">
-                <div class="account_form" data-aos="fade-up" data-aos-delay="0">
-                    <h3>login</h3>
-                    <form action="#" method="POST">
-                        <div class="default-form-box">
-                            <label>Username or email <span>*</span></label>
-                            <input type="text">
-                        </div>
-                        <div class="default-form-box">
-                            <label>Passwords <span>*</span></label>
-                            <input type="password">
-                        </div>
-                        <div class="login_submit">
-                            <button class="btn btn-md btn-black-default-hover mb-4" type="submit">login</button>
-                            <label class="checkbox-default mb-4" for="offer">
-                                <input type="checkbox" id="offer">
-                                <span>Remember me</span>
-                            </label>
-                            <a href="#">Lost your password?</a>
-
-                        </div>
-                    </form>
-                </div>
-            </div> --}}
-            <!--login area start-->
-
-            <!--register area start-->
-            {{-- <div class="col-lg-6 col-md-6">
-                <div class="account_form register" data-aos="fade-up" data-aos-delay="200">
-                    <h3>Register</h3>
-                    <form action="#">
-                        <div class="default-form-box">
-                            <label>Email address <span>*</span></label>
-                            <input type="text">
-                        </div>
-                        <div class="default-form-box">
-                            <label>Passwords <span>*</span></label>
-                            <input type="password">
-                        </div>
-                        <div class="login_submit">
-                            <button class="btn btn-md btn-black-default-hover" type="submit">Register</button>
-                        </div>
-                    </form>
-                </div>
-            </div> --}}
-            <!--register area end-->
-            <div class="col-md-12">
-                <div class="">
-                    <ul class="nav nav-pills mb-3 d-flex justify-content-center" id="pills-tab" role="tablist">
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Connexion</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Inscription</button>
-                        </li>
-                    </ul>
-                    <div class="tab-content" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                            <form>
-                                <div class="row d-flex justify-content-center">
-                                    <div class="col-md-6 shadow-lg p-3 mb-5 bg-white rounded">
-                                        <div class="default-form-box">
-                                            <label>Email <span class="text-danger">*</span></label>
-                                            <input type="text">
-                                        </div>
-                                        <div class="default-form-box">
-                                            <label>Mot de passe <span class="text-danger">*</span></label>
-                                            <input type="password" >
-                                        </div>
-                                        <div class="login_submit">
-                                            <button class="btn btn-md btn-black-default-hover mb-4" type="submit">Se connecter</button>                        
-                                        </div>
-                                    </div>
+                <!--register area start-->
+                <div class="col-lg-8 col-md-8">
+                    <div class="account_form register">
+                        <h3>Inscription</h3>
+                        <form wire.igore wire:submit.prevent="store">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Prenom <span class="text-danger">*</span></label>
+                                        <input type="text" class="@error('form1.prenom') is-invalid
+                                        @enderror" placeholder="Prénom" wire:model="form1.prenom">
+                                        @error('form1.prenom') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
                                 </div>
-                            </form>
-                        </div>
-                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                            
-                        </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Nom <span class="text-danger">*</span></label>
+                                        <input type="text" class="@error('form1.nom') is-invalid
+                                        @enderror" placeholder="Nom" wire:model="form1.nom">
+                                        @error('form1.nom') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Nationnalité <span class="text-danger">*</span></label>
+                                        <input type="text" class="@error('form1.nationalite') is-invalid
+                                        @enderror" placeholder="nationalite" wire:model="form1.nationalite">
+                                        @error('form1.nationalite') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Pays <span class="text-danger">*</span></label>
+                                        <input type="text" class="@error('form1.pays') is-invalid
+                                        @enderror" placeholder="Pays" wire:model="form1.pays">
+                                        @error('form1.pays') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Pseudo</label>
+                                        <input type="text" class="@error('form1.pseudo') is-invalid
+                                        @enderror" placeholder="Pseudo" wire:model="form1.pseudo">
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Adresse </label>
+                                        <input type="text" class="@error('form1.adresse') is-invalid
+                                        @enderror" placeholder="Adresse" wire:model="form1.adresse">
+                                        @error('form1.adresse') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label> Téléphone 1<span class="text-danger">*</span></label>
+                                        <input type="text" class="@error('form1.tel') is-invalid
+                                        @enderror" placeholder="Télephone 1" wire:model="form1.tel">
+                                        @error('form1.tel') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Téléphone 2 </label>
+                                        <input type="text" class="@error('form1.tel2') is-invalid
+                                        @enderror" placeholder="Téléphone 2" wire:model="form1.tel2">
+                                        @error('form1.tel2') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Email <span class="text-danger">*</span></label>
+                                        <input type="email" class="@error('form1.email') is-invalid
+                                        @enderror" placeholder="Email" wire:model="form1.email">
+                                        @error('form1.email') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-form-box">
+                                        <label>Mot de passe <span class="text-danger">*</span></label>
+                                        <input type="password" class="@error('form1.password') is-invalid
+                                        @enderror" placeholder="Mot de passe" wire:model="form1.password">
+                                        @error('form1.password') <span class="error text-danger">{{$message}}</span> @enderror
+                                    </div>  
+                                </div>
+                                <div>
+                                    <button style="background-color: #ff365d; color: white" class="btn btn-md mb-4 float-left" type="reset">Annuler</button>  
+                                    <button class="btn btn-md btn-black-default-hover mb-4 float-right" type="submit" wire:click.prevent="store()">S'inscrire</button>  
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-               
+                <!--register area end-->
             </div>
         </div>
-    </div>
-</div> <!-- ...:::: End Customer Login Section :::... -->
+    </div> <!-- ...:::: End Customer Login Section :::... -->
 
 </div>
