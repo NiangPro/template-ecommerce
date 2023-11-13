505018041da97f1192193b47aeeb8e5e1342ae1d
NiangPro <NiangProgrammeur@gmail.com>
2023-11-13 12:16:14 +0000

    etat

--- app/Http/Livewire/Profil.php
+++ app/Http/Livewire/Profil.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Profil extends Component
 {
+    public $form = [
+        "id" => null,
+        "nom" => "",
+        "prenom" => "", 
+        "adresse" => "",
+        "nationalite" => "",
+        "pseudo" => "",
+        "pays" => "",
+        "image" => null,
+        "role" => "client",
+        "tel" => "",
+        "tel2" => null,
+        "email" => "",
+        "password" => "",
+        "password_confirmation" => "",
+    ];
+
     public function render()
     {
         return view('livewire.admin.profil')->layout("layouts.dashboard");
@@ -17,5 +35,19 @@ class Profil extends Component
         if (!Auth()->user() || !Auth()->user()->isAdmin()) {
             return redirect(route("accueil"));
         }
+
+        $u = User::where("id", Auth::user()->id)->first();
+
+        $this->form["nom"] = $u->nom;
+        $this->form["id"] = $u->id;
+        $this->form["prenom"] = $u->prenom;
+        $this->form["email"] = $u->email;
+        $this->form["tel"] = $u->tel;
+        $this->form["tel2"] = $u->tel2;
+        $this->form["nationalite"] = $u->nationalite;
+        $this->form["adresse"] = $u->adresse;
+        $this->form["pays"] = $u->pays;
+        $this->form["pseudo"] = $u->pseudo;
+        $this->imgEditing = $u->image;
     }
 }
--- resources/views/livewire/admin/profil.blade.php
+++ resources/views/livewire/admin/profil.blade.php
@@ -23,6 +23,67 @@
         <div class="card radius-10 border-warning border-start border-0 border-4 col-md-7">
             <div class="card-body">
                 <h5 class="text-center">Information Personnelle</h5><hr>
+                <form action="" method="post">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <img width="180" class="col-md-12 rounded" height="200" src="{{asset('storage/images/'.Auth::user()->image)}}" alt="user avatar">
+                            <div class="form-group mt-2 mb-4 col-md-12">
+                                <input type="file" placeholder="Entrer l'image du produit" class="form-control @error('form.image') is-invalid @enderror" wire:model="form.image">
+                                @error('form.image') <span class="error text-danger">{{$message}}</span> @enderror
+                                <div wire:loading wire:target="form.image" class="text-success">Chargement...</div>
+                            </div>
+                        </div>
+                        <div class="col-md-7 mt-4">
+                            <div class="form-group mb-4 col-md-12">
+                                <label for="">Prénom <span class="text-danger">*</span></label>
+                                <input type="text" placeholder="Entrer le prénom" class="form-control @error('form.prenom') is-invalid @enderror" wire:model="form.prenom">
+                                @error('form.prenom') <span class="error text-danger">{{$message}}</span> @enderror
+                            </div>
+                            <div class="form-group mb-4 col-md-12">
+                                <label for="">Nom <span class="text-danger">*</span></label>
+                                <input type="text" placeholder="Entrer le nom" class="form-control @error('form.nom') is-invalid @enderror" wire:model="form.nom">
+                                @error('form.nom') <span class="error text-danger">{{$message}}</span> @enderror
+                            </div>
+                            <div class="form-group mb-4 col-md-12">
+                                <label for="">Nom utilisateur (pseudo) <span class="text-danger">*</span></label>
+                                <input type="text" placeholder="Entrer le nom d'utilisateur" class="form-control @error('form.pseudo') is-invalid @enderror" wire:model="form.pseudo">
+                                @error('form.pseudo') <span class="error text-danger">{{$message}}</span> @enderror
+                            </div>
+                        </div>
+                        <div class="form-group mb-4 col-md-12">
+                            <label for="">Adresse <span class="text-danger">*</span></label>
+                            <input type="text" placeholder="Entrer l'adresse" class="form-control @error('form.adresse') is-invalid @enderror" wire:model="form.adresse">
+                            @error('form.adresse') <span class="error text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="form-group mb-4 col-md-6">
+                            <label for="">Numéro téléphone 1 <span class="text-danger">*</span></label>
+                            <input type="text" placeholder="Ex: 783828282" class="form-control @error('form.tel') is-invalid @enderror" wire:model="form.tel">
+                            @error('form.tel') <span class="error text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="form-group mb-4 col-md-6">
+                            <label for="">Numéro téléphone 2</label>
+                            <input type="text" placeholder="Ex: 783828282" class="form-control @error('form.tel2') is-invalid @enderror" wire:model="form.tel2">
+                            @error('form.tel2') <span class="error text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="form-group mb-4 col-md-12">
+                            <label for="">Email <span class="text-danger">*</span></label>
+                            <input type="email" placeholder="Entrer l'adresse email" class="form-control @error('form.email') is-invalid @enderror" wire:model="form.email">
+                            @error('form.email') <span class="error text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="form-group mb-4 col-md-6">
+                            <label for="">Pays</label>
+                            <input type="text" placeholder="Entrer le pays" class="form-control @error('form.pays') is-invalid @enderror" wire:model="form.pays">
+                            @error('form.pays') <span class="error text-danger">{{$message}}</span> @enderror
+                        </div>
+                        <div class="form-group mb-4 col-md-6">
+                            <label for="">Nationalite</label>
+                            <input type="text" placeholder="Entrer la nationalité" class="form-control @error('form.nationalite') is-invalid @enderror" wire:model="form.nationalite">
+                            @error('form.nationalite') <span class="error text-danger">{{$message}}</span> @enderror
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-outline-warning">Modifier</button>
+
+                </form>
             </div>
         </div>
     </div>
