1d1e7b74231951144454cd422a6817992408b0c8
Fall codeur <fallmidadi1996@gmail.com>
2023-12-15 10:51:05 +0000

    all design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,14 +90,18 @@
                                     </div><!-- .End .input-group-append -->
                                 </div><!-- .End .input-group -->
                             </form>
-                            <div class="custom-control custom-checkbox">
+                            {{-- <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="register-policy-2" required>
                                 <label class="custom-control-label" for="register-policy-2">Do not show this popup again</label>
-                            </div><!-- End .custom-checkbox -->
+                            </div><!-- End .custom-checkbox --> --}}
                         </div>
                     </div>
                     <div class="col-xl-2-5col col-lg-5 ">
-                        <img src="{{asset('assets/images/popup/newsletter/img-1.jpg')}}" class="newsletter-img" alt="newsletter">
+                        @foreach($menupubs as $key=>$p)
+                            @if($key == 2)
+                                <a href="produit/{{$p->product->id}}"><img src="{{asset('storage/images/'.$p->product->image)}}" class="newsletter-img" alt="newsletter"></a>
+                            @endif
+                        @endforeach
                     </div>
                 </div>
             </div>
